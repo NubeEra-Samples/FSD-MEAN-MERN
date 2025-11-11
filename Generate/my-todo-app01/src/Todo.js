21fd04e38aede562557c8777365cc42fbae268a2
mujahed85 <mujahed.mca@gmail.com>
2025-11-11 22:11:36 +0530

    V1.2 -  Updated Todo.js Files with Add Task

---
+++ Generate/my-todo-app01/src/Todo.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function Todo() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = () => {
+    if (newTask.trim()) {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  return (
+    <div className="todo">
+      <h2>Todo List</h2>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={handleAddTask}>Add Task</button>
+      
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
