8c6763f2c3bac123096433a8d3985c12967cd41a
Syed Mujahed Hussaini <mujahed.mca@gmail.com>
2025-11-24 10:48:40 +0530

    Refactor server.js to use PostgreSQL for data insertion

--- 22.ReadWriteFiles/server.js
+++ 22.ReadWriteFiles/server.js
@@ -1,7 +1,8 @@
 // server.js
 const http = require('http');
 const readCSV = require('./readCSV'); // Import the readCSV function
-const appendToCSV = require('./writeCSV'); // Import the appendToCSV function
+// const appendToCSV = require('./writeCSV'); // Import the appendToCSV function
+const insertIrisRow = require('./writeToDB');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
@@ -11,21 +12,41 @@ const server = http.createServer((req, res) => {
     res.end(csvContent);
   }
 
+  // else if (req.method === 'POST') {
+  //   let body = '';
+
+  //   req.on('data', chunk => body += chunk);
+
+  //   req.on('end', () => {
+  //     try {
+  //       const data = JSON.parse(body); // expect JSON input
+  //       appendToCSV(data);
+
+  //       res.writeHead(200, { "Content-Type": "application/json" });
+  //       res.end(JSON.stringify({ status: "Row added to CSV" }));
+  //     } catch (err) {
+  //       res.writeHead(400, { "Content-Type": "application/json" });
+  //       res.end(JSON.stringify({ error: "Invalid JSON" }));
+  //     }
+  //   });
+  // }
+
   else if (req.method === 'POST') {
     let body = '';
 
     req.on('data', chunk => body += chunk);
 
-    req.on('end', () => {
+    req.on('end', async () => {
       try {
-        const data = JSON.parse(body); // expect JSON input
-        appendToCSV(data);
+        const data = JSON.parse(body);
+
+        await insertIrisRow(data);
 
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ status: "Row added to CSV" }));
+        res.end(JSON.stringify({ status: "Row added to PostgreSQL" }));
       } catch (err) {
         res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Invalid JSON" }));
+        res.end(JSON.stringify({ error: "Invalid JSON or DB error" }));
       }
     });
   }
